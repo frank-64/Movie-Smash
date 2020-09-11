1086dbf19545fab2238a12457a0409067e37d167
b9028176 <f.clipsham1@newcastle.ac.uk>
2020-09-11 16:30:01 +0100

    Testing some code I previously had written before the git issue. This will take the movies.csv file, parse it, then insert it into the sqlite3 database.

--- movie_smash/views.py
+++ movie_smash/views.py
@@ -4,10 +4,10 @@ from .models import Movies
 
 
 def index(request):
-    # with open('movie_smash/movies.csv', 'r') as file:
-    #     reader = csv.reader(file)
-    #     for row in reader:
-    #         m = Movies(movieID=row[1], title=row[2], release_date=row[0], popularity=0)
-    #         m.save()
+    with open('movie_smash/movies.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            m = Movies(movieID=row[1], title=row[2], release_date=row[0], popularity=0)
+            m.save()
     query_results = Movies.objects.all()
     return render(request, 'index.html', {'query_results':query_results})
