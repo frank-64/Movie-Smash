baf840e1f04d4c188f830b37820c611d3fe0c456
b9028176 <f.clipsham1@newcastle.ac.uk>
2020-09-25 22:49:46 +0100

    Changed index.html file to be suitable for the movie smash style of website. This displays two random movies next to each other.

--- movie_smash/views.py
+++ movie_smash/views.py
@@ -1,14 +1,18 @@
 import csv
 from django.shortcuts import render
 from .models import Movies
-
+import random
 
 def index(request):
+    # parse the movies.csv file into the database
     # with open('./movies.csv', 'r') as file:
     #     reader = csv.reader(file)
     #     for row in reader:
     #         m = Movies(movieID=row[1], title=row[2], release_year=row[0])
     #         m.save()
     #
+
     query_results = Movies.objects.all()
-    return render(request, 'index.html', {'query_results':query_results})
+    m1 = query_results[random.randint(0, 500)]
+    m2 = query_results[random.randint(0, 500)]
+    return render(request, 'index.html', {'m1': m1, 'm2': m2})
